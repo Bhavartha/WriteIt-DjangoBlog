9d867ed0e642e10a0cd81558b20139fdc88947f0
Bhavartha Khawale <bhavarthakhawale@gmail.com>
2020-12-19 16:50:02 +0530

    Added html for showing flash messages

--- users/views.py
+++ users/views.py
@@ -1,4 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 def register(request):
-    pass
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            messages.success(request,f'Account for {username} created!')
+            return redirect('blog-home')
+    else:
+        form = UserCreationForm()
+    context = {'form': form}
+    return render(request, 'users/register.html', context)
