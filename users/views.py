956327dc1f3110cb3f924247a8dea775c6fd7dca
Bhavartha Khawale <bhavarthakhawale@gmail.com>
2020-12-20 21:31:10 +0530

    profile page

--- users/views.py
+++ users/views.py
@@ -22,3 +22,6 @@ def register(request):
     
     context = {'form': form}
     return render(request, 'users/register.html', context)
+
+def profile(request):
+    return render(request,'users/profile.html')
